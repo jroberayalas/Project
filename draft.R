e3468da4252fd471f05971e5be7f8bfe6d38da5b
ichbinjras <ichbinjras@gmail.com>
2015-02-17 16:17:03 +0000

    Add draft.R
    Code for model construction

---
+++ draft.R
@@ -0,0 +1,24 @@
+# Read data
+dataset <- read.csv("pml-training.csv", na.strings = c("NA", "", " "))
+
+# Check for NAs
+sum(!complete.cases(dataset))
+summary(dataset)
+
+# Omit columns with NAs
+omit.variables <- sapply(dataset, function(x) {sum(is.na(x)) > 0})
+dataset <- dataset[ , !omit.variables]
+
+# Omit first columns (no useful info)
+library(dplyr)
+dataset <- select(dataset, -(X:num_window))
+
+# Create training and test set
+library(caret)
+inTrain <- createDataPartition(dataset$classe, p = 0.7, list = FALSE)
+training <- dataset[inTrain, ]
+testing <- dataset[-inTrain, ]
+
+# Training Random Forest
+model.RF <- train(classe ~ ., data = training, method = "rf", prox = TRUE)
+model.Boost <- train(classe ~ ., data = training, method = "gbm", verbose = FALSE)
